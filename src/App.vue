ffce55f91db866aa0575cd7055818ba357ac7326
Guillaume Perbet <perbet.dev@gmail.com>
2020-10-18 11:32:59 +0200

    add logo

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,18 @@
       app
       color="primary"
       dark
+      class="mb-10"
     >
-      
     </v-app-bar>
 
     <v-main>
+      <!-- <v-container>
+        <v-row>
+          <v-col cols="3" offset="9"> -->
+            <v-img class="float-right mr-10" width="250" src="@/assets/logo-DM-SAI.png"></v-img>
+            <!-- </v-col> -->
+        <!-- </v-row>
+      </v-container> -->
       <Tree/>
     </v-main>
   </v-app>
